058ac36b243795f3fdcd0cdb1d99db8d6cfe0580
Ruturaj Sant <sant.ruturaj2001@gmail.com>
2022-10-12 00:23:14 +0530

    fix

--- push.js
+++ push.js
@@ -5,4 +5,5 @@ let vapid = {
 }
 
 push.setVapidDetails('mailto:sant.ruturaj2001@gmail.com', vapid.publicKey, vapid.privateKey)
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/eAWyx1fe4Do:APA91bEe8aMY8BOL_p0uWxjBkJm03Or77EScWQbLOo8DlkBs6h7pstPYPlQHMzM2flqzK6GxLS-ucvb1_CVfZJD66BXabcVy-DMpYlMGIvr7RsuTrqE-Fg1JWJeZ6gaac6JyMv2C5unU", "expirationTime": null, "keys": { "p256dh": "BINjwiHaCV9Miya-HTU0_qYNCLNRhCwtcXypfCKSjlVVcjUAe6qwXOuGtprWjSLxh0IuO6_y8tkwY_qTwaP66l4", "auth": "kIPD7Ye3uCfmETQ4IbuPqQ" } }
 push.sendNotification(sub, "test message")
